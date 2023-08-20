e8bde0589a28ffb5c5934fa4e7c5887e15825fed
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-08-20 13:48:11 -0600

    first commit

---
+++ js/javaScript.js
@@ -0,0 +1,6 @@
+let cadena = "convertir a mayusculas";
+
+console.log("Cadena minuscula :", cadena);
+
+console.log("Cadena mayuscula :", cadena.toUpperCase());
+//Convertiremos las letras a mayusculas con el metodo touppercase()
